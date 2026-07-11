e363ec63e6daadc182c0548310b9904f21dbe270
ELUMBA-maker <elumba237@gmail.com>
2026-07-11 14:50:44 +0100

    introduction into backend concepts

---
+++ 1_intro_nodejs/index.js
@@ -0,0 +1,29 @@
+// console.log("Hello world");
+// const weight=67;
+// const height=154;
+// const BMI =weight/(height* height);
+
+// console.log(BMI);
+
+const fs=require("fs/promises");
+
+async function main(){
+    // write a file
+    await fs.writeFile("doc.txt", "welcome to backend");  
+   
+// how to read a file 
+const content=await fs.readFile("./doc.txt","utf-8");
+console.log(content);
+// read files ina directory
+const files=await fs.readdir(".");
+console.log(files);
+// delete a file
+await fs.unlink("./doc.txt")
+
+
+}
+main();
+
+
+
+
---
+++ 1_intro_nodejs/server.js
@@ -0,0 +1,35 @@
+const http=require('http');
+// we are going to be using http to create our first srrver
+
+console.log(http)
+const PORT=3000;
+// create the server 
+const userData=[
+    {
+        name:"Paul",
+        email:"paul@gmail.com",
+        description:"blabla"
+    },
+    {
+        name:"Anna",
+        email:"anna@gmail.com",
+        description:"blabla"
+    }
+]
+
+const server=http.createServer((req,res)=>{
+console.log(`Recieved ${req.method} request for ${req.url}`);
+res.writeHead(200,{
+    "content-type":"application/json"
+});
+
+res.end(JSON.stringify(userData));
+console.log(userData);
+
+// send data above to the client using the res object
+})
+
+server.listen(PORT, ()=>{
+console.log(`server running on port: ${PORT}`)
+
+})
