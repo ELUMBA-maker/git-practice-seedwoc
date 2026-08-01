704b2b449e57bf74c1a298fcb867a22578f37813
ELUMBA-maker <elumba237@gmail.com>
2026-08-01 23:05:28 +0100

    creating routes

--- 1_intro_nodejs/index.js
+++ 1_intro_nodejs/index.js
@@ -5,24 +5,24 @@
 
 // console.log(BMI);
 
-const fs=require("fs/promises");
+// const fs=require("fs/promises");
 
-async function main(){
-    // write a file
-    await fs.writeFile("doc.txt", "welcome to backend");  
+// async function main(){
+//     // write a file
+//     await fs.writeFile("doc.txt", "welcome to backend");  
    
-// how to read a file 
-const content=await fs.readFile("./doc.txt","utf-8");
-console.log(content);
-// read files ina directory
-const files=await fs.readdir(".");
-console.log(files);
-// delete a file
-await fs.unlink("./doc.txt")
+// // how to read a file 
+// const content=await fs.readFile("./doc.txt","utf-8");
+// console.log(content);
+// // read files ina directory
+// const files=await fs.readdir(".");
+// console.log(files);
+// // delete a file
+// await fs.unlink("./doc.txt")
 
 
-}
-main();
+// }
+// main();
 
 
 
