5c3d590e39bdcc98b53aba53f2e5840a5f1cdf72
ELUMBA-maker <elumba237@gmail.com>
2026-08-05 22:32:10 +0100

    creating and connecting to database

--- 1_intro_nodejs/config/env.js
+++ 1_intro_nodejs/config/env.js
@@ -3,4 +3,4 @@ import path from "node:path";
 
 config ({path: `.env.${process.env.NODE_ENV || `development`}.local`});
 
-export const {port,NODE_ENV}= process.env;
+export const {port,NODE_ENV,DB_URI}= process.env;
